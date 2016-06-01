a6dbef12f833471a47204637b7159c9ca25bc68b
Padam Bhandari <Padam.Bhandari@datascan.com>
2016-06-01 10:35:51 -0500

    Added a Gig form

---
+++ Gighub/Controllers/GigsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gighub.Controllers
+{
+    using System.Web.Razor;
+
+    using Gighub.Models;
+    using Gighub.ViewModels;
+
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+           var viewModel = new GigFormViewModel()
+                               {
+                                   Genres = _context.Genres.ToList()
+                               };
+
+            return View(viewModel);
+        }
+    }
+}
